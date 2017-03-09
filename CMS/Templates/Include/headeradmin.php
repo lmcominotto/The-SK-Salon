cfa193863073e6b6c96890762104221ad8144887
Haley-H <haley.m.hardwick@gmail.com>
2017-03-09 06:42:30 -0600

    Updated Include folder and Stylesheet folder
    Updated include folder with headeradmin, login.inc.php, logout.inc.php,
    and signup.inc.php.  Updated Stylesheets folder to include mainadmin.

---
+++ CMS/Templates/Include/headeradmin.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title><?php echo htmlspecialchars( $title )?></title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel ="stylesheet" href="stylesheets/mainadmin.css">
+		<link href="https://fonts.googleapis.com/css?family=Josefin+Sans|Rokkitt|Rochester" rel="stylesheet">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	</head>	
+	<body>
+		<div class="outerContainer">
+			<div id = "topName">
+				<img src = "img/sk.png" alt = "The SK Salon">
+			</div>
