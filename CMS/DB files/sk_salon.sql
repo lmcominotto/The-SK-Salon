7fea17426dbebff8311468526e7c433f72f55fc9
Haley-H <haley.m.hardwick@gmail.com>
2017-03-17 10:19:34 -0500

    Admin Portal - DB Files and Photos Update
    Added my DB files for testing purposes; added templates for photos
    action forms (both delete and insert); updated photos page to dynamic.

--- CMS/DB files/sk_salon.sql
+++ CMS/DB files/sk_salon.sql
@@ -1,21 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Mar 08, 2017 at 12:53 PM
--- Server version: 10.1.19-MariaDB
--- PHP Version: 5.6.28
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
 -- Database: `sk salon`
 --
@@ -61,17 +43,19 @@ INSERT INTO `siteuser` (`id`, `firstName`, `lastName`, `userID`, `password`) VAL
 --
 
 CREATE TABLE `stylists` (
+  `ID` int(11) NOT NULL,
   `Title` text NOT NULL,
-  `Text` text NOT NULL
+  `Text` text NOT NULL,
+  `image` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `stylists`
 --
 
-INSERT INTO `stylists` (`Title`, `Text`) VALUES
-('Sandee Kranz', 'Sandee Kranz, owner of The SK Salon, has been working as a hair stylist for ___ years. 	A graduate of Paul Mitchell The School Huntsville, Sandee worked at Bangs Salon, The Stile Salon as Master Stylist, and now as an independent stylist for The SK Salon in New Beginnings Salon and	Trading Post located in Athens, AL.'),
-('Lorem Ipsum', 'Lorem ipsum dolor sit amet, lobortis phasellus enim, sed vestibulum volutpat tellus condimentum sed sed, ante sit proin consequat amet ultrices, arcu bibendum scelerisque. Aenean fames, viverra a in, libero elementum ac gravida vulputate vitae, sem ipsum wisi lorem dolor tristique in, pede elit est eu luctus. Inceptos erat. Cubilia id metus accumsan eu, commodo commodo vel, nisl in, elementum ac, neque ac posuere velit. Blandit in erat nunc eleifend. Scelerisque montes wisi mi.');
+INSERT INTO `stylists` (`ID`, `Title`, `Text`, `image`) VALUES
+(1, 'Sandee Kranz', 'Sandee Kranz, owner of The SK Salon, has been working as a hair stylist for ___ years. A graduate of Paul Mitchell The School Huntsville, Sandee worked at Bangs Salon, The Stile Salon as Master Stylist, and now as an independent stylist for The SK Salon located in Athens, AL.', 'img/skranz.jpg'),
+(2, 'Lorem Ipsem', 'This is more text', 'img/carousel2.jpg');
 
 --
 -- Indexes for dumped tables
@@ -84,6 +68,12 @@ ALTER TABLE `siteuser`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `stylists`
+--
+ALTER TABLE `stylists`
+  ADD PRIMARY KEY (`ID`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -92,6 +82,11 @@ ALTER TABLE `siteuser`
 --
 ALTER TABLE `siteuser`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `stylists`
+--
+ALTER TABLE `stylists`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- CMS/Templates/Photos.php
+++ CMS/Templates/Photos.php
@@ -1,9 +1,24 @@
-<?php include 'Include/header.php'; ?>
+<?php 
+$title = "Photo Gallery";
+include 'include/header.php'; 
+include 'dbh.php';?>
 				
-				<div>
-					<ul id = "gallery">
-						<li><img src = "img/img1.jpeg" alt = "image 1"></li>
-						<li><img src = "img/img2.jpg"></li>
+	<div>
+		<ul id = "gallery">
+			<?php
+				$sql = "SELECT * FROM photos";
+				$result = mysqli_query($conn, $sql);
+				$count = $result->num_rows;
+							
+				if ($count !=0)
+			{
+				while($row = $result->fetch_object()){ ?>
+						<li><img src = "<?php echo $row->Image; ?>"></li>
+			<?php
+				}
+			}
+			?>
+				<!--		<li><img src = "img/img2.jpg"></li>
 						<li><img src = "img/img3.jpeg"></li>
 						<li><img src = "img/img4.jpg"></li>
 						<li><img src = "img/img5.jpeg"></li>
@@ -17,8 +32,10 @@
 						<li><img src = "img/img13.jpeg"></li>
 						<li><img src = "img/img14.jpeg"></li>
 						<li><img src = "img/img15.jpg"></li>
-						<li><img src = "img/img16.jpg"></li>
+						<li><img src = "img/img16.jpg"></li>-->
 					</ul> 
 				</div>
 				
-<?php include 'Include/footer.php'; ?>
+				<script src = "lightbox.js"></script>
+				
+<?php include 'include/footer.php'; ?>
