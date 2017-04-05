bdf027e5f99557cfec76c3bc4cdca14fea1bbe22
Michelle <lmcominotto@gmail.com>
2017-04-04 19:16:48 -0500

    Create footer.php

---
+++ CMS In Progress/includes/footer.php
@@ -0,0 +1,22 @@
+			</div>
+		</div>
+		
+		<script>
+			function myFunction() {
+				var x = document.getElementById("myTopnav");
+				if (x.className === "topnav") {
+					x.className += " responsive";
+				} else {
+					x.className = "topnav";
+				}
+			}
+		</script>
+	
+		<footer class="footer">
+			<p class="bottomaddress">
+				Copyright 2017 The SK Salon
+				<a href = "admin.php"> | Site Admin</a>
+			</p>
+		</footer>
+	</body>
+</html>
