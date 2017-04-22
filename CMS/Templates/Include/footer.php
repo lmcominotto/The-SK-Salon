282005fe8b52598be597f05ce6b79a36a339ded6
Haley-H <haley.m.hardwick@gmail.com>
2017-04-22 09:02:07 -0500

    Cleaned up repo
    Moved items into appropriate folders and deleted old files that were not
    used.

--- CMS/Templates/Include/footer.php
+++
@@ -1,9 +0,0 @@
-<div class="footer">
-						<p class="bottomaddress">
-							<br>Copyright 2017 The SK Salon          <a href= "admin.php">Site Admin</a>
-						</p>
-					</div>
-				</div>
-		</div>
-	</body>
-</html>
--- CMS/readme.txt
+++
@@ -1,1 +0,0 @@
-This folder contains all necessary PHP, CSS, and database files for the website as they were intended for production.
