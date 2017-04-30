efef1abb94b87a31ab433194cdc35571ec16296e
Michelle <lmcominotto@gmail.com>
2017-04-30 12:43:53 -0500

    Create footer.php

---
+++ CMS Final/includes/footer.php
@@ -0,0 +1,21 @@
+			</div>
+		</div>
+		
+		<script>
+			function myFunction() {
+				var x = document.getElementById("myTopnav");
+				if (x.className === "topnav") {
+					x.className += " responsive";
+				} else {
+					x.className = "topnav";
+				}
+			}
+		</script>
+	
+		<footer class="footer">
+			<p class="bottomaddress">
+				The SK Salon &copy 2017
+			</p>
+		</footer>
+	</body>
+</html>
